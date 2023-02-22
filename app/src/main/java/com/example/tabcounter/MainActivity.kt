9cdc6104a8b40ecfd5a9e564b5ec2e8adafc46e8
Minh Hien Le <85823911+minkkun@users.noreply.github.com>
2023-02-21 21:29:18 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tabcounter/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.tabcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener {
+           // Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+        }
+
+
+    }
+}
